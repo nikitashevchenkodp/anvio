35580ad42909f33b156d2f89fb2b2a6c82fa5b7b
Nikita Shevchenko <nrcnekita9@gmail.com>
2022-08-17 15:04:03 +0200

    crashed slider

---
+++ webpack.config.js
@@ -0,0 +1,74 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = ({
+    development
+}) => ({
+    mode: development ? "development" : "production",
+    entry: {
+        index: "./src/index.js",
+    },
+    output: {
+        filename: "[name].[contenthash].js",
+        assetModuleFilename: "asset/[hash][ext][query]",
+        clean: true,
+    },
+    devtool: development ? "source-map" : "inline-source-map",
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+        }),
+        new MiniCssExtractPlugin({
+            filename: "[name].[contenthash].css",
+        }),
+        new ESLintPlugin({
+            extensions: ["js"],
+            exclude: ["/node_modules/", "/bower_components/", "/dist"],
+        }),
+    ],
+    module: {
+        rules: [{
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    development ? "style-loader" : MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    {
+                        loader: "postcss-loader",
+                        options: {
+                            postcssOptions: {
+                                plugins: [
+                                    ["postcss-preset-env"]
+                                ],
+                            },
+                        },
+                    },
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"],
+                    },
+                },
+            },
+        ],
+    },
+    optimization: {
+        splitChunks: {
+            chunks: "all",
+        },
+    },
+});
